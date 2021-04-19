1edb1d4015bf55256ce56e14443b1f852c465ff5
Willem Van der Elst <28760191+n00bhax@users.noreply.github.com>
2021-04-19 10:54:06 +0200

    Initial commit

---
+++ src/main/java/Person.java
@@ -0,0 +1,21 @@
+public class Person {
+
+    private String name;
+    private double balance = 0;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+}
---
+++ src/main/java/Server.java
@@ -0,0 +1,9 @@
+import static spark.Spark.*;
+
+public class Server {
+    public static void main(String[] args) {
+
+        get("/hello", (req, res) -> "Hello World");
+
+    }
+}
